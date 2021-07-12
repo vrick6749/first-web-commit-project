68f39978137cda11a54d86c7ffb8ef7f33ac9a10
Rick <vrick6749@gmail.com>
2021-07-12 17:34:29 +1000

    conditional if else at ternary operator

--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
-<<<<<<< HEAD
-var x= "joe"
-
-console.log(x)
-=======
-let x= {name:"JOHN", lastname:"DOE"}
-console.log(x.name)
+// let x= {name:"JOHN", lastname:"DOE"}
+// console.log(x.name)
 
 
-let y =["apple","mango", "bannana"]
+// let y =["apple","mango", "bannana"]
 
-y.forEach(item => func(item))
+// y.forEach(item => func(item))
 
-function func(item){
-    console.log("Happy "+ item + " day")
+// function func(item){
+//     console.log("Happy "+ item + " day")
+// }
+
+function functest(p1,p2){
+    return (p1>5 ? console.log(p1*p2) : console.log("the first number must be higher than 5"))
 }
->>>>>>> 2a73a7a067af0c23f4dfcc1aa449cfe7302cd59b
+
+functest(6,5)
