33808eeb2ff2202bc616811b89437603a5741086
Rick <vrick6749@gmail.com>
2021-07-17 14:10:57 +1000

    object revision

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
-let x= {
-    firstname:"John",
-    lastName:"Doe",
-    age:14,
-    id:1233,
-    fullname: function(){
-        return this.firstname + " " + this.lastName
-    }
-}
+// let x= {
+//     firstname:"John",
+//     lastName:"Doe",
+//     age:14,
+//     id:1233,
+//     fullname: function(){
+//         return this.firstname + " " + this.lastName
+//     }
+// }
 
+
+
+// console.log(`Welcome ${x.firstname} ${x.lastName}`)
