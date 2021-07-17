b7891d648fcbf9d4ccd972e0936e70a8bf6a69e0
Rick <vrick6749@gmail.com>
2021-07-17 15:36:01 +1000

    messing with functions

--- javascript.js
+++ javascript.js
@@ -11,7 +11,19 @@
 
 
 // console.log(`Welcome ${x.firstname} ${x.lastName}`)
-let car = ["BMW", "Mercedes", "Toyota"]
-for(let i=0; i<car.length; i++){
-    console.log(car[i])
-}
+// let car = ["BMW", "Mercedes", "Toyota"]
+// for(let i=0; i<car.length; i++){
+//     console.log(car[i])
+// }
+
+// let person ={
+//     firstname: "Rick",
+//     lastname: "Vazquez",
+//     age:27
+// }
+
+// let x = 0
+// while(x<5){
+//     console.log("ME")
+//     x++
+// } 
